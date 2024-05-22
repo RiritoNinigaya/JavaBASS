eb676e2d16f1d0ddbfe8d462f172da98bcff664b
Ririto Ninigaya(Ryo Tabuki) <riritoninigaya@hotmail.com>
2024-05-22 11:43:01 +0300

    Fixed Initializating BASS Library
    Fixed Initializating BASS Library(Example)

--- SourceCode/ExampleCode/JavaBASS-Example/src/Main.java
+++ SourceCode/ExampleCode/JavaBASS-Example/src/Main.java
@@ -2,6 +2,7 @@ import com.sun.jna.LastErrorException;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
 import com.sun.jna.Platform;
+import com.sun.jna.Pointer;
 import com.sun.jna.win32.StdCall;
 import com.sun.jna.win32.StdCallLibrary;
 
@@ -11,17 +12,19 @@ public class Main
     {
         boolean BASS_Start() throws LastErrorException;
 
-        boolean BASS_Init(int device, int freq, int flags, int win, int dsguid) throws LastErrorException;
+        boolean BASS_Init(int device, int freq, int flags, Pointer win, Pointer dsguid) throws LastErrorException;
         boolean BASS_Stop() throws LastErrorException; //Stoping BASS :>
     }
     public static final BASS INSTANCE = Platform.isWindows() ? Native.load("bass.dll", BASS.class) : null;
 
-    public static void Bass_Start(){
+    public static void Bass_Start()
+    {
+        INSTANCE.BASS_Init(-1, 48000, 0, Pointer.NULL, Pointer.NULL);
         if(INSTANCE.BASS_Start()){
             System.out.printf("BASS IS STARTED!!!");
         }
         else{
-            System.out.printf("Bass Is Not Founded");
+            System.out.printf("Bass Is Not Founded or BASS Is Not Initializated");
         }
     }
     public static void main(String[] args) {
